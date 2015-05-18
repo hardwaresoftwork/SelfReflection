48a0972c4208066e353d5e85e0a8cc5de5287187
willpowerstudios <creation@willpowerstudios.com>
2015-05-18 22:25:17 +0200

    Update Camera
    Uses Laptop Camera Automatically
    Added my logo

--- build-tmp/source/SelfReflection.java
+++ build-tmp/source/SelfReflection.java
@@ -1,3 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.*; 
+import ddf.minim.*; 
+import ddf.minim.analysis.*; 
+import processing.pdf.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SelfReflection extends PApplet {
+
 // Created 2015.05.05
 // by William Ismael / WILLPOWER STUDIOS
 // Open Source Software
@@ -8,12 +29,12 @@
 ////////////////////
 
 
-import processing.video.*;
 
-import ddf.minim.*;
-import ddf.minim.analysis.*;
 
-import processing.pdf.*;
+
+
+
+
 
 
 ////////////////////
@@ -36,7 +57,7 @@ PImage WillpowerIdentity;
 
 ////////////////////
 
-void setup() {
+public void setup() {
   size( 1440,850 ); 
   background( 0 );
   smooth();
@@ -59,9 +80,9 @@ void setup() {
     }
     
     // Detecting MacBook Pro's camera
-    cam = new Capture(this, cameras[0]);
+    //cam = new Capture(this, cameras[0]);
     // Using USB Logitech webcam
-    //cam = new Capture( this, 1440, 900, "Logitech Camera", 30);
+    cam = new Capture( this, 1440, 900, "Logitech Camera", 30);
     cam.start();     
   }      
 
@@ -70,7 +91,7 @@ void setup() {
   WillpowerIdentity = loadImage("WILLPOWER-STUDIOS-logo.png");
 }
 
-void draw() {
+public void draw() {
 
   if (record) {
     beginRecord(PDF, "frame-####.pdf"); 
@@ -436,6 +457,15 @@ if ( key=='3' ) {
 }
 
 // Generate Vector Art
-void mousePressed() {
+public void mousePressed() {
   record = true;
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--full-screen", "--bgcolor=#000000", "--hide-stop", "SelfReflection" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
